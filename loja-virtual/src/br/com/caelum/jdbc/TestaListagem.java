17ae55f40d3905386225734ac464df0f6c91865e
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-03-01 14:57:55 -0400

    [ADD] projeto base e comunicação com o base de dados

---
+++ loja-virtual/src/br/com/caelum/jdbc/TestaListagem.java
@@ -0,0 +1,28 @@
+package br.com.caelum.jdbc;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TestaListagem {
+	public static void main(String[] args) throws SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/loja-virtual", "SA", "");
+		
+		Statement statement = (Statement) connection.createStatement();
+		boolean resultado = statement.execute("select * from Produto");
+		ResultSet resultSet = statement.getResultSet();
+		while(resultSet.next()) {
+			int id = resultSet.getInt("id");	
+			String nome = resultSet.getString("nome");
+			String descricao = resultSet.getString("descricao");
+			System.out.println(id);
+			System.out.println(nome);
+			System.out.println(descricao);
+		}
+		resultSet.close();
+		statement.close();
+		connection.close();
+	}
+}
